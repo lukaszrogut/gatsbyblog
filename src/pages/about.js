63f9843dcd30dad4c5eba7dd58e78779d083fe88
Łukasz Rogut <lukasz.rogut@makolab.com>
2021-10-15 07:22:28 +0200

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+const AboutPage = () => {
+    return(
+        <main>
+            <title>About Page</title>
+            <h1>About me</h1>
+            <p>Hi, my name is Lukasz Rogut</p>
+            <p>Currently I'm into:</p>
+            <ul>
+                <li>React</li>
+                <li>TypeScript</li>
+                <li>Books</li>
+                <li>Sim Racing</li>
+                <li>Meditation</li>
+                <li>Cycling</li>
+            </ul>
+        </main>
+    )
+}
+
+export default AboutPage
+
