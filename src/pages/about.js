59bba44f749da5de0f073133a05be36e6fe87bd1
Łukasz Rogut <lukasz.rogut@makolab.com>
2021-10-16 08:31:07 +0200

    added layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
 import * as React from "react"
-import {Link} from "gatsby"
-import IndexPage from "."
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return(
-        <main>
-            <title>About Page</title>
-            <Link to="/">go back</Link>
-            <h1>About me</h1>
+        <Layout 
+            pageTitle="about page" 
+            pageHeading="About Me"
+        >
             <p>Hi, my name is Lukasz Rogut</p>
             <p>Currently I'm into:</p>
             <ul>
@@ -18,7 +17,7 @@ const AboutPage = () => {
                 <li>Meditation</li>
                 <li>Cycling</li>
             </ul>
-        </main>
+        </Layout>
     )
 }
 
